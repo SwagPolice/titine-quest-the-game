06244c0f0f383e32b9b464a4a43f6ba3378635d0
Côme Delobelle <come.delobelle@gmail.com>
2026-07-11 17:22:47 +0200

    draft for rulebook

---
+++ complete-version/boards/board-rules.js
@@ -0,0 +1,65 @@
+// 📝 KALBLAST room EFFECTS CONFIGURATION
+// Line index matches the room Number directly (Index 0 = Start, Index 60 = Victory)
+const roomTexts = [
+  "", // room 0
+  "Everybody drinks", // room 1
+  "Player to your left drinks", // room 2
+  "Player in front of you drinks", // room 3
+  "Go to room 28", // room 4
+  "You drink (bitch)", // room 5
+  "<b>RPS</b> versus another player, loser drinks one <b>Š</b>", // room 6
+  "Drink and move a ladder", // room 7
+  "Give out one sip to another player", // room 8
+  "Whisper until your next turn, drink when you fail", // room 9
+  "Guys drink, if no guy, everybody drinks", // room 10
+  "Five squats or five push-ups or two <b>Š</b>", // room 11
+  "Drink one quarter of your glass", // room 12
+  "Player to your right drinks", // room 13
+  "Collective <b>RPS</b>", // room 14
+  "<b>Suitcase</b>", // room 15
+  "Drink as many sips as the dice just did", // room 16
+  "Fill up your glass, drink as much as you want, another player has to drink as much", // room 17
+  "Roll 1d6: drink that many sips if even, give them if odd", // room 18
+  "Drink and take an extra turn", // room 19
+  "Girls drink, if no girls, everybody drinks", // room 20
+  "Drink a <b>Š</b> ♥", // room 21
+  "You can't speak until your next turn. Drink when you do", // room 22
+  "<b>X or Y</b>", // room 23
+  "You are the <b>Queen of Questions</b>", // room 24
+  "Drink water", // room 25
+  "People with even ages drink", // room 26
+  "People with odd ages drink", // room 27
+  "Drink and make up a permanent rule", // room 28
+  "Choose another player to be <b>bound</b> with", // room 29
+  "Give two sips to two people", // room 30
+  "Drink and go to room 50", // room 31
+  "Drink a shot of hard liquor", // room 32
+  "Take a priest token for free", // room 33
+  "Player with the fullest drink drinks two sips", // room 34
+  "Rotate classes clockwise. Complainers drink", // room 35
+  "Trade glasses with a player, you both drink half of it, then switch back", // room 36
+  "<b>Never have I ever</b>", // room 37
+  "Switch positions of <b>Š</b> bottles as you want, then drink a <b>Š</b> from any bottle", // room 38
+  "Drink and go to room 31", // room 39
+  "<b>According to...</b>", // room 40
+  "Swap seats with the player opposite you. You are now <b>bound</b>", // room 41
+  "<b>Is this anecdote true or false?</b>", // room 42
+  "One shot of hard liquor with someone, or two <b>Š</b> alone", // room 43
+  "Drink water", // room 44
+  "Until your next turn, finish your sentences by \"Yeah\", drink when you forget", // room 45
+  "People with glasses more than half full drink", // room 46
+  "Until your next turn, <b>little man in your glass</b>. One <b>Š</b> when you forget", // room 47
+  "Drink and activate your ability for free next time", // room 48
+  "Trade your class with the player in front of you. Complainers drink", // room 49
+  "Trigger Clazgreb's ability for free, except you can get hit as well", // room 50
+  "Move 3 rooms ahead and drink", // room 51
+  "Place a Carnila landmine in that room", // room 52
+  "Roll 2d6 to move forward that many rooms and drink that many sips", // room 53
+  "Drop a rule made in room 32. Their creator drinks. If there is no rule, you drink", // room 54
+  "Royal <b>RPS</b>. Losers drink. If you win everything, give one <b>Š</b>", // room 55
+  "Player with the emptiest glass empties it", // room 56
+  "Drink 2 sips and go forward one room OR drink 1 <b>Š</b> and take an extra turn", // room 57
+  "Drink water", // room 58
+  "<b>Would you rather</b>", // room 59
+  "You win, all players except you chug their glasses, you may give yours to another player. <br><br>UNBREACHABLE: NO ladder on it" // room 60
+];
